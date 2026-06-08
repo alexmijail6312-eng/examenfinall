8eb18dc2bc563382994661ce5ff454f54ba98b74
alexmijail6312 <alexmijail6312@gmail.com>
2026-06-08 09:29:31 -0400

    componente Remada curvada realizado

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,13 @@ const exercises: Exercise[] = [
     reps: 12,
     imageUrl: "/6897dd537959d0801b173c66ceb0647dc37baea6.jpg",
   },
+  {
+    id: "2",
+    name: "Remada curvada",
+    sets: 3,
+    reps: 12,
+    imageUrl: "/9af6513a3c39d123e3225f56f18849b34f0ab91d.png",
+  },
 ];
 
 export default function Home() {
