63efc779fc02143ca74a7573b551bc004a4b75bc
alexmijail6312 <alexmijail6312@gmail.com>
2026-06-08 09:28:34 -0400

    componente ChevronRightIcon realizado

---
+++ app/components/atoms/ChevronRightIcon.tsx
@@ -0,0 +1,19 @@
+export default function ChevronRightIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={20}
+      height={20}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="text-[#636366] flex-shrink-0"
+      aria-hidden="true"
+    >
+      <path d="M9 18l6-6-6-6" />
+    </svg>
+  );
+}
